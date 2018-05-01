22dc0934ce0e054f9e73e9b76095803ab0339aba
Revath S Kumar <rsk@revathskumar.com>
2018-05-01 16:14:23 +0530

    feat(Save to localStorage) : save and show success and error notification

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,12 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Html exposing (Html, text, div, label, input, p, section, span, i, a, button, nav)
-import Html.Attributes exposing (src, class, type_, value)
+import Html.Attributes exposing (src, class, type_, value, classList)
 import Html.Events exposing (onClick, onInput, on)
+import Random
 import Task
+import Json.Decode
+import Json.Encode
 import List.Extra
 import Item
 
@@ -11,15 +14,25 @@ import Item
 ---- MODEL ----
 
 
+type NotificationType
+    = Error
+    | Success
+
+
+type alias Notification =
+    { type_ : NotificationType, message : String }
+
+
 type alias Model =
-    { items : List Item.Item, nextId : Int, hoursPerDay : Int }
+    { items : List Item.Item, nextId : Int, hoursPerDay : Int, id : Int, notification : Notification }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { items = [], nextId = 2, hoursPerDay = 8 }
+    ( { items = [], nextId = 2, hoursPerDay = 8, id = 0, notification = { type_ = Success, message = "" } }
     , Cmd.batch
-        [ Task.succeed (AddItem "Setup & provision production server") |> Task.perform identity
+        [ Random.generate NewId (Random.int 1000 10000)
+        , Task.succeed (AddItem "Setup & provision production server") |> Task.perform identity
         , Task.succeed (AddItem "Setup CI") |> Task.perform identity
         , Task.succeed (AddItem "Setup Project Skeleton") |> Task.perform identity
         ]
@@ -34,11 +47,70 @@ type Msg
     = AddItem String
     | UpdateHoursPerDay String
     | ItemAction Int Item.Msg
+    | SaveEstimate
+    | NewId Int
+    | UpdateNotification Notification
+    | ClearNotification
+
+
+modelToValue : Model -> Json.Encode.Value
+modelToValue model =
+    Json.Encode.object
+        [ ( "items", Json.Encode.list (List.map itemToValue model.items) )
+        , ( "nextId", Json.Encode.int model.nextId )
+        , ( "id", Json.Encode.int model.id )
+        , ( "hoursPerDay", Json.Encode.int model.hoursPerDay )
+        ]
+
+
+itemToValue : Item.Item -> Json.Encode.Value
+itemToValue item =
+    Json.Encode.object
+        [ ( "description", Json.Encode.string item.description )
+        , ( "hours", Json.Encode.int item.hours )
+        , ( "id", Json.Encode.int item.id )
+        , ( "checked", Json.Encode.bool item.checked )
+        ]
+
+
+decodeNotification : Json.Decode.Decoder Notification
+decodeNotification =
+    Json.Decode.map2 Notification
+        (Json.Decode.field "type" Json.Decode.string
+            |> Json.Decode.andThen decodeNotificationType
+        )
+        (Json.Decode.field "message" Json.Decode.string)
+
+
+decodeResponse : Json.Decode.Value -> Msg
+decodeResponse json =
+    case Json.Decode.decodeValue decodeNotification json of
+        Result.Ok notification ->
+            UpdateNotification notification
+
+        Result.Err _ ->
+            UpdateNotification { type_ = Success, message = "" }
+
+
+decodeNotificationType : String -> Json.Decode.Decoder NotificationType
+decodeNotificationType ntype =
+    case ntype of
+        "Error" ->
+            Json.Decode.succeed Error
+
+        "Success" ->
+            Json.Decode.succeed Success
+
+        _ ->
+            Json.Decode.fail ""
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        NewId id ->
+            ( { model | id = id }, Cmd.none )
+
         AddItem description ->
             let
                 initItem =
@@ -58,6 +130,22 @@ update msg model =
                     String.toInt hours |> Result.toMaybe |> Maybe.withDefault 0
             in
                 ( { model | hoursPerDay = hoursPerDay }, Cmd.none )
+
+        SaveEstimate ->
+            ( model, saveEstimate (modelToValue model) )
+
+        UpdateNotification notification ->
+            ( { model | notification = notification }, Cmd.none )
+
+        ClearNotification ->
+            let
+                notification =
+                    model.notification
+
+                updatedNotification =
+                    { notification | message = "" }
+            in
+                ( { model | notification = updatedNotification }, Cmd.none )
 
         ItemAction id childAction ->
             case childAction of
@@ -111,14 +199,23 @@ view model =
         div []
             [ section [ class "section" ]
                 [ div [ class "container " ]
-                    [ nav [ class "level" ]
+                    [ (if (model.notification.message /= "") then
+                        div [ classList [ ( "notification", True ), ( "is-danger", model.notification.type_ == Error ), ( "is-success", model.notification.type_ == Success ) ] ]
+                            [ button [ class "delete", onClick ClearNotification ] []
+                            , text model.notification.message
+                            ]
+                       else
+                        div [] []
+                      )
+                    , nav [ class "level" ]
                         [ div [ class "level-left" ]
                             [ span [ class "panel-icon icon-font" ] [ i [ class "fas fa-stopwatch" ] [] ]
                             , span [ class "panel-text" ] [ text "Working hours/day" ]
                             , span [ class "panel-text column is-2" ] [ input [ type_ "number", class "input", toString model.hoursPerDay |> value, onInput UpdateHoursPerDay ] [] ]
                             ]
                         , div [ class "level-right" ]
-                            [ button [ class "button is-primary", onClick (AddItem "") ] [ text "Add Item" ]
+                            [ div [ class "level-item" ] [ button [ class "button is-primary", onClick (AddItem "") ] [ text "Add Item" ] ]
+                            , div [ class "level-item" ] [ button [ class "button is-success", onClick (SaveEstimate) ] [ text "Save Estimate" ] ]
                             ]
                         ]
                     , div [ class "panel" ]
@@ -148,11 +245,22 @@ view model =
 ---- PROGRAM ----
 
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    receiveNotification decodeResponse
+
+
+port saveEstimate : Json.Encode.Value -> Cmd msg
+
+
+port receiveNotification : (Json.Decode.Value -> msg) -> Sub msg
+
+
 main : Program Never Model Msg
 main =
     Html.program
         { view = view
         , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import './main.css';
-import { Main } from './Main.elm';
+import {Main} from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Main.embed(document.getElementById('root'));
+const estimateHelperApp = Main.embed (document.getElementById ('root'));
 
-registerServiceWorker();
+estimateHelperApp.ports.saveEstimate.subscribe (estimate => {
+  if (window.localStorage) {
+    console.log (estimate);
+    try {
+      const data = JSON.parse (localStorage.getItem ('estimate-helper')) || {};
+      data[estimate.id] = estimate;
+      localStorage.setItem ('estimate-helper', JSON.stringify (data));
+      estimateHelperApp.ports.receiveNotification.send ({
+        type: 'Success',
+        message: 'Saved successfully.',
+      });
+    } catch (e) {
+      estimateHelperApp.ports.receiveNotification.send ({
+        type: 'Error',
+        message: 'Unexpected Error :: ' + e.message,
+      });
+    }
+  } else {
+    alert ('localStorage is not supported');
+    estimateHelperApp.ports.receiveNotification.send ({
+      type: 'Error',
+      message: 'Error : localStorage is not supported.',
+    });
+  }
+});
+
+registerServiceWorker ();
